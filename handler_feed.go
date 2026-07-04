c1ff42a770fd0ef5093147e97f4fff2c9a49bfee
HP <hemsleypichardo@gmail.com>
2026-07-04 15:24:37 -0400

    Split feed queries to feeds and feed_follow. Can unfollow feeds now

--- handler_feed.go
+++ handler_feed.go
@@ -109,3 +109,26 @@ func handlerListFeedFollows(s *state, cmd command, user database.User) error {
 
 	return nil
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Unfollow requires a feed url")
+	}
+
+	feedUrl := cmd.args[0]
+
+	feed, err := s.db.GetFeedByURL(context.Background(), feedUrl)
+	if err != nil {
+		return err
+	}
+
+	err = s.db.UnfollowFeed(context.Background(), database.UnfollowFeedParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
