415c98f15e33e199c4bd5fd74f5811b0998dd1fc
HP <hemsleypichardo@gmail.com>
2026-07-06 12:28:04 -0400

    marks when feed was last fetched and aggregates now based on a given time

--- handler_agg.go
+++ handler_agg.go
@@ -3,19 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handlerAggregate(s *state, cmd command) error {
-	ctx := context.Background()
-	givenFeed := "https://www.wagslane.dev/index.xml"
 
-	feed, err := fetchFeed(ctx, givenFeed)
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Expected time arugment")
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("Invalid time argument: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	defer ticker.Stop()
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	next_feed, err := s.db.GetNextFeedToFetch(ctx)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%+v\n", feed)
+	err = s.db.MarkFeedFetched(ctx, next_feed.ID)
+	if err != nil {
+		return err
+	}
+
+	feed, err := fetchFeed(ctx, next_feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range feed.Channel.Items {
+		fmt.Println(item.Title)
+	}
 
 	return nil
-
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,14 @@ SELECT feeds.name AS RSS_Name, feeds.url, users.name AS username FROM feeds LEFT
 
 -- name: GetFeedByURL :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(),
+    updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
